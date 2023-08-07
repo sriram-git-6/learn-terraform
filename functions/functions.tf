1363fbaf0abeec3675fcfa9766928ba19606b5cf
sriram <sriram.mudunuru@gmail.com>
2023-08-07 19:19:15 +0530

    terraform

--- functions/functions.tf
+++ functions/functions.tf
@@ -6,3 +6,12 @@ output "fruits"{
   #value = var.fruits[3]
   value = element(var.fruits, 3)
 }
+
+variable "fruits_with_stock" {
+  default = {
+    apple = 100
+  }
+}
+output "fruits_with_stock"{
+  value = try(var.fruits_with_stock["banana"],0)
+}
