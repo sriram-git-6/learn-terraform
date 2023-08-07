c216b0da96efa8ebdfb5ddd0698a7dfe9bb4b255
sriram <sriram.mudunuru@gmail.com>
2023-08-07 07:48:56 +0530

    terraform

--- variable-sample/var-sample.tf
+++ variable-sample/var-sample.tf
@@ -14,29 +14,29 @@
 #}
 
 # map variable, plain
-variable "fruit_stock" {
-  default = {
-    apple = 100
-    banana = 50
-  }
-}
-output "fruit_stock" {
-  value = var.fruit_stock.apple
-}
-
-#map variable, map of maps
-#variable "fruit_stock_price"{
+#variable "fruit_stock" {
 #  default = {
-#    apple = {
-#      stock = 50
-#      price = 200
-#    }
-#    banana = {
-#      stock = 50
-#      price = 50
-#    }
+#    apple = 100
+#    banana = 50
 #  }
 #}
-#output "fruit_stock_price" {
-#  value = var.fruit_stock_price
+#output "fruit_stock" {
+#  value = var.fruit_stock
 #}
+
+#map variable, map of maps
+variable "fruit_stock_price"{
+  default = {
+    apple = {
+      stock = 50
+      price = 200
+    }
+    banana = {
+      stock = 50
+      price = 50
+    }
+  }
+}
+output "fruit_stock_price" {
+  value = var.fruit_stock_price
+}
