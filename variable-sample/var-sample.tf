cdffe29e9d8d3796c68a4ea2050f805247335ada
sriram <sriram.mudunuru@gmail.com>
2023-08-07 15:41:03 +0530

    terraform

--- variable-sample/var-sample.tf
+++ variable-sample/var-sample.tf
@@ -7,7 +7,11 @@ output "fruit_name" {
 
 # list variable
 variable "fruits"{
-  default = [ "apple", "banana" ]
+  default = [
+    "apple",
+    "banana"
+    ]
+#  default = [ "apple", "banana" ]
 }
 output "fruits" {
   value = [var.fruits[0], var.fruits[1]]
@@ -41,4 +45,22 @@ variable "fruit_stock_price"{
 }
 output "fruit_stock_price_of_apple" {
   value = var.fruit_stock_price.apple.stock
+#  value = var.fruit_stock_price[apple].stock
+}
+
+# variable datatypes
+
+variable "fruit_details"{
+  default = {
+    apple = {
+      stock = 100
+      type = "washington"
+      for_sale = true
+    }
+  }
+}
+
+output "fruit_details" {
+  value = "apple stock = ${var.fruit_details[apple].stock}, apple type = ${var.fruit_details[apple].type}, apple sale status = ${var.fruit_details[apple].for_sale}"
+
 }
