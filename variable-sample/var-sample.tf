78b52231a8d592042f42224e47682e0a3b38f05c
sriram <sriram.mudunuru@gmail.com>
2023-08-07 12:24:41 +0530

    terraform

--- variable-sample/var-sample.tf
+++ variable-sample/var-sample.tf
@@ -1,17 +1,17 @@
-#variable "fruit_name"{
-#  default = "apple"
-#}
-#output "fruit_name" {
-#  value = var.fruit_name
-#}
+variable "fruit_name"{
+  default = "apple"
+}
+output "fruit_name" {
+  value = var.fruit_name
+}
 
 # list variable
-#variable "fruits"{
-#  default = [ "apple", "banana" ]
-#}
-#output "fruits" {
-#  value = [var.fruits[0], var.fruits[1]]
-#}
+variable "fruits"{
+  default = [ "apple", "banana" ]
+}
+output "fruits" {
+  value = [var.fruits[0], var.fruits[1]]
+}
 
 ## map variable, plain
 variable "fruit_stock" {
@@ -27,18 +27,18 @@ output "fruit_stock_apple" {
 }
 
 #map variable, map of maps
-#variable "fruit_stock_price"{
-#  default = {
-#    apple = {
-#      stock = 50
-#      price = 200
-#    }
-#    banana = {
-#      stock = 50
-#      price = 50
-#    }
-#  }
-#}
-#output "fruit_stock_price" {
-#  value = var.fruit_stock_price
-#}
+variable "fruit_stock_price"{
+  default = {
+    apple = {
+      stock = 50
+      price = 200
+    }
+    banana = {
+      stock = 50
+      price = 50
+    }
+  }
+}
+output "fruit_stock_price_of_apple" {
+  value = var.fruit_stock_price.apple.stock
+}
