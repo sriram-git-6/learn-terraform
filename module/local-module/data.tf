56fb5469f88b4302abd81e572080c9464a3ebecc
sriram <sriram.mudunuru@gmail.com>
2023-08-19 05:22:04 +0530

    terraform

--- module/local-module/data.tf
+++ module/local-module/data.tf
@@ -1,16 +1,16 @@
-data "aws_ami_ids" "ami" {
+data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["777414146682"]
 }
 
 output "ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami.ami
 }
 
 resource "aws_instance" "instance" {
-  ami = data.aws_ami_ids.ami.id
-  instance_type = "t3.micro"
+  ami = data.aws_ami.ami.id
+  instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-01c1b8d157ecd6541" ]
 
 }
